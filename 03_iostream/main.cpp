f496de5c457dbddacb965c7c03f4edfebe44ea55
Phil Shaw <phil.shaw@gmx.com>
2021-08-14 20:05:44 -0500

    20210814_2005

--- 03_iostream/main.cpp
+++ 03_iostream/main.cpp
@@ -3,8 +3,11 @@
 int main() {
 	std::cout << "Enter a number: "; // ask user for a number
 
-	int x{ }; // define variable x to hold user input (and zero-initialize it)
-	std::cin >> x; // get number from keyboard and store it in variable x
+	// define variable x to hold user input (and zero-initialize it)
+	int x{ };                                 
+
+	// get number from keyboard and store it in variable x
+	std::cin >> x;
 
 	std::cout << "You entered " << x << '\n';
 
