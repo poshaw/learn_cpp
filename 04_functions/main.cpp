3d80e4116f051b3f3eb7f8f755145f6d64b67246
Phil Shaw <poadshaw@gmail.com>
2021-08-15 21:59:04 -0500

    20210815_2159

--- 04_functions/main.cpp
+++ 04_functions/main.cpp
@@ -8,21 +8,21 @@ int getValueFromUser() {
 	return input;
 }
 
-int add(int x, int y)
-{
+int add(int x, int y) {
 	return x + y;
 }
  
-int multiply(int z, int w)
-{
+int multiply(int z, int w) {
 	return z * w;
 }
 
 int main( [[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
 	int num1 { getValueFromUser() };
 	int num2 { getValueFromUser() };
-	std::cout << num1 << " + " << num2 << " = " << add( num1, num2 ) << '\n';
-	std::cout << num1 << " x " << num2 << " = " << multiply( num1, num2 ) << '\n';
+	std::cout << num1 << " + " << num2 << " = "
+		<< add( num1, num2 ) << '\n';
+	std::cout << num1 << " x " << num2 << " = "
+		<< multiply( num1, num2 ) << '\n';
 
 	return 0;
 }
