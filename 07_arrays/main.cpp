18e8f80b14cf501e84ef82636ba0f55d2ee3ed6d
Phil Shaw <poadshaw@gmail.com>
2021-08-23 16:03:22 -0500

    20210823_1603

--- 07_arrays/main.cpp
+++ 07_arrays/main.cpp
@@ -1,10 +1,18 @@
-#include <iostream>
+#include<iostream>
+#include<iterator>
 
 int main( [[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
 	// define & initialize a variable
-	int a{ 0 }; // define the variable a as an int and set it's value to 0
+	// note:  arrays indexes have type unsigned long int per std::size()
+	double list[5] = { 0.0, 1.2, 2.1, 3.6, 4.5 }; // create an array of 5 doubles, and initialize their values
+	
 	// print a
-	std::cout<<"The value of a is: " << a << '\n';
+	std::cout<<"The length of list is: " << std::size(list) << '\n';
+
+	// print the elemnts in an array
+	for ( unsigned long int i = 0; i < std::size(list); i++ ) {
+		std::cout<< "list[" << i << "] = " << list[i] << '\n';
+	}
 
 	return 0;
 }
